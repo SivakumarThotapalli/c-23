58dbeb5af0b93a21296b7b87269ba7a4d21ac032
SivakumarThotapalli <74361262+SivakumarThotapalli@users.noreply.github.com>
2020-11-21 19:05:35 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box{
+    constructor(x,y,width,height){
+    var options={
+    restitution:1.0
+    }
+    this.width=width;
+    this.height=height
+    this.body=Bodies.rectangle(x,y,width,height,options);
+    World.add(world,this.body)
+    }
+    display(){
+    var pos=this.body.position;
+    rectMode(CENTER);
+    fill("white");
+    rect(pos.x,pos.y,this.width,this.heights)    
+    }
+}
